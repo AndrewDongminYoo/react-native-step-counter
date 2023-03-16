c78b28cf8f7d0666e04c3590b7c2e9518a6ce555
Dongmin,Yoo <drew.yoo@gbike.io>
2023-03-16 15:37:05 +0900

    ci(react): 💚 some changes related to CI
    some changes such as scripts related to pre-installation and deployment, some dependency packages, and one optional gradle.property

---
+++ scripts/clean.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import { sync as deleteSync } from 'del';
+
+const patterns = [
+  '!**/node_modules',
+  'example/android/.gradle',
+  'android/.gradle',
+  'example/(android|ios)/**/build',
+  'example/android/app/.cxx',
+  'example/android/app/(release|debug)',
+  'example/(android|ios)/**/main.jsbundle',
+  'example/ios/DerivedData',
+  'example/ios/Pods',
+];
+
+const options = {
+  force: true,
+  dryRun: true,
+  onProgress(progress) {
+    const { deletedFiles, totalFiles, percent } = progress;
+    const deleteFileCount = deletedFiles.toLocaleString();
+    const totalFileCount = totalFiles.toLocaleString();
+    const percentComplete = (percent * 100).toFixed(1);
+    console.log(`${deleteFileCount}/${totalFileCount} files deleted (${percentComplete} %)`);
+  },
+};
+
+deleteSync(patterns, options);
