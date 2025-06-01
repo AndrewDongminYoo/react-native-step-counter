70979566abe8bbe9bc9e4c20de3707216db9590e
Dongmin Yu <ydm2790@gmail.com>
2025-06-01 20:51:22 +0900

    feat(example): specify platforms for react-native config
    This commit explicitly specifies the `ios` and `android` platforms within the `react-native.config.js` file. This is done to prevent the codegen script from failing, which occurs when the platforms are not explicitly defined. The platforms are defined with empty objects.

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,10 +1,16 @@
 const path = require('path');
-const pak = require('../package.json');
+const pkg = require('../package.json');
 
 module.exports = {
   dependencies: {
-    [pak.name]: {
+    [pkg.name]: {
       root: path.join(__dirname, '..'),
+      platforms: {
+        // Codegen script incorrectly fails without this
+        // So we explicitly specify the platforms with empty object
+        ios: {},
+        android: {},
+      },
     },
   },
 };
