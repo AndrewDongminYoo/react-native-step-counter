155511f098bf67dc21a961c2d9d6c49dfdf8ca15
Dongmin Yu <ydm2790@gmail.com>
2025-06-01 17:58:11 +0900

    fix: relax commitlint rules
    This commit relaxes some of the commitlint rules to allow for more flexibility in commit messages.
    
    The following changes were made:
    - Removed the formatter configuration.
    - Removed the parserPreset configuration.
    - Changed `body-max-line-length` rule from error to off.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,10 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  formatter: './node_modules/@commitlint/format/lib/index.js',
   helpUrl:
     'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
-  parserPreset: 'conventional-changelog-conventionalcommits',
   rules: {
     'body-leading-blank': [1, 'always'],
-    'body-max-line-length': [2, 'always', 100],
+    'body-max-line-length': [0, 'never', 100],
     'footer-leading-blank': [1, 'always'],
     'footer-max-line-length': [2, 'always', 100],
     'header-max-length': [2, 'always', 100],
