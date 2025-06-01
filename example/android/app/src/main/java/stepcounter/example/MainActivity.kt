7f7d5e9958a49db96bddf87b7731cfa334b288d4
Dongmin Yu <ydm2790@gmail.com>
2025-06-01 19:10:56 +0900

    refactor(example): rename example Android app package
    This commit refactors the example Android application by renaming the package from `com.stepcounterexample` to `stepcounter.example`. This change aligns the package name with the project's naming conventions and improves code organization.
    
    The following changes were made:
    
    - Renamed the package in `MainApplication.kt` from `com.stepcounterexample` to `stepcounter.example`.
    - Renamed the package in `MainActivity.kt` from `com.stepcounterexample` to `stepcounter.example`.

--- example/android/app/src/main/java/stepcounter/example/MainActivity.kt
+++ example/android/app/src/main/java/stepcounter/example/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.stepcounterexample
+package stepcounter.example
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- example/android/app/src/main/java/stepcounter/example/MainApplication.kt
+++ example/android/app/src/main/java/stepcounter/example/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.stepcounterexample
+package stepcounter.example
 
 import android.app.Application
 import com.facebook.react.PackageList
