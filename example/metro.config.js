afe852788a55038fe5080b02d424aab863e88181
Dongmin, Yu <ydm2790@gmail.com>
2026-05-24 11:12:08 +0900

    chore: ⬆️ update example app dependencies and configurations
    This commit updates the following dependencies:
    - @react-native/jest-preset: added at 0.85.3
    
    It also updates `jest.config.js` to use `@react-native/jest-preset` and refactors `metro.config.js` to leverage `mergeConfig` from `@react-native/metro-config`. These changes align the example application's configuration with current React Native tooling practices.

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,18 +1,18 @@
 const path = require("path");
-const { getDefaultConfig } = require("@react-native/metro-config");
+const { getDefaultConfig, mergeConfig } = require("@react-native/metro-config");
 const { withMetroConfig } = require("react-native-monorepo-config");
 
 const root = path.resolve(__dirname, "..");
 
 /**
  * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
+ * https://reactnative.dev/docs/metro
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('@react-native/metro-config').MetroConfig}
  */
 const config = withMetroConfig(getDefaultConfig(__dirname), {
   root,
   dirname: __dirname,
 });
 
-module.exports = config;
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
